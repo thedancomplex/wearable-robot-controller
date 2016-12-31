e1f5bbdd2ca2a3c6966211526dee3b7fc1b8476d
Daniel M. Lofaro <dan@danlofaro.com>
2016-12-30 23:36:24 -0500

    just started the project

---
+++ inc/wearable-robot-controller.h
@@ -0,0 +1,19 @@
+#ifndef __wearable-robot-controller_H__
+#define __wearable-robot-controller_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "wearable-robot-controller"
+
+#if !defined(PACKAGE)
+#define PACKAGE "com.lofarolabs.wearable-robot-controller"
+#endif
+
+#endif /* __wearable-robot-controller_H__ */
